3a2f7c4887def2b42d1a4ad6d34dbf76902c1c44
ISEATO-JR <julio.rivera.1596@gmail.com>
2023-10-14 22:37:40 -0600

    Conexxion DATABASE PostgreSQL

--- TiendaOnline/TiendaOnline/settings.py
+++ TiendaOnline/TiendaOnline/settings.py
@@ -74,13 +74,23 @@ WSGI_APPLICATION = 'TiendaOnline.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+       'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'articuloclientes',
+        'USER': 'postgres',
+        'PASSWORD': 'iseatosql',
+        'HOST': '127.0.0.1',
+        'DATABASES_PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
