f1b8322fb9bc2546dcbab44f82617df0e1e67b93
ISEATO-JR <julio.rivera.1596@gmail.com>
2023-11-22 22:57:36 -0600

    Update Config

--- TiendaOnline/GestionPedidos/admin.py
+++ TiendaOnline/GestionPedidos/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from GestionPedidos.models import Cliente,Articulo,Pedido
 
+#Clase para ver los otros campos de nuerto modelo.
+class ClienteAdmin(admin.ModelAdmin):
+    list_display=("nombre","direccion","telefono") #Nuestra los campos que necesitamos
+    search_fields=("nombre","telefono")                            #Muestra un campo de busqueda al que referimos para realizar la consulta
+
 # Register your models here.
-admin.site.register(Cliente)
+admin.site.register(Cliente,ClienteAdmin)
 admin.site.register(Articulo)
 admin.site.register(Pedido)
 
--- TiendaOnline/GestionPedidos/models.py
+++ TiendaOnline/GestionPedidos/models.py
@@ -3,9 +3,12 @@ from django.db import models
 # Create your models here.
 class Cliente(models.Model):
     nombre = models.CharField(max_length=30)
-    direccion = models.CharField(max_length=50)
+    direccion = models.CharField(max_length=50, verbose_name="La direccion")
     email = models.EmailField(blank=True, null=True)
     telefono = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.nombre
 
 class Articulo(models.Model):
     nombre = models.CharField(max_length=30)
