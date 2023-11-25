0e3d164a3abe5736ce7130da8760c1cd036d7032
ISEATO-JR <julio.rivera.1596@gmail.com>
2023-11-24 22:36:34 -0600

    Fomularios

--- TiendaOnline/GestionPedidos/admin.py
+++ TiendaOnline/GestionPedidos/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from GestionPedidos.models import Cliente,Articulo,Pedido
 
-#Clase para ver los otros campos de nuerto modelo.
+#Clase para ver los otros campos de nuestro modelo.
 class ClienteAdmin(admin.ModelAdmin):
     list_display=("nombre","direccion","telefono") #Nuestra los campos que necesitamos
     search_fields=("nombre","telefono")                            #Muestra un campo de busqueda al que referimos para realizar la consulta
