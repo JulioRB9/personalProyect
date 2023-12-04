078c0cc5ad7f0ad4f7c0733c738fa7cc097d9098
ISEATO-JR <julio.rivera.1596@gmail.com>
2023-12-03 23:37:49 -0600

    Config Site

---
+++ MiWeb/ProyectoWebApp/urls.py
@@ -0,0 +1,11 @@
+# URLS de proyecto
+from django.urls import path      # Path para importar la APP
+from ProyectoWebApp import views  # inportar las vistas de la aplicacion
+
+urlpatterns = [
+    path('',views.inicio, name="Inicio" ),
+    path('servicios/',views.servicios, name="Servicios" ),
+    path('tienda/',views.tienda, name="Tienda" ),
+    path('blog',views.blog, name="Blog" ),
+    path('contacto/',views.contacto, name="Contacto" ),
+]
---
+++ MiWeb/ProyectoWebApp/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def inicio(request):
+    return render(request, 'ProyectoWebApp/inicio.html')
+
+# Create your views here.
+def servicios(request):
+    return render(request, 'ProyectoWebApp/servicios.html')
+
+# Create your views here.
+def tienda(request):
+    return render(request, 'ProyectoWebApp/tienda.html')
+
+# Create your views here.
+def blog(request):
+    return render(request, 'ProyectoWebApp/blog.html')
+
+# Create your views here.
+def contacto(request):
+    return render(request, 'ProyectoWebApp/contactos.html')
+
+
