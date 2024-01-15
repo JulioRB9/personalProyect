e9e313f4b54da852235cdfad707ea06e766b1c8c
ISEATO-JR <julio.rivera.1596@gmail.com>
2024-01-15 00:25:52 -0600

    Config Site

--- MiWeb/ProyectoWebApp/urls.py
+++ MiWeb/ProyectoWebApp/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('',views.inicio, name="Inicio" ),
     path('servicios/',views.servicios, name="Servicios" ),
     path('tienda/',views.tienda, name="Tienda" ),
-    path('blog',views.blog, name="Blog" ),
+    path('blog/',views.blog, name="Blog" ),
     path('contacto/',views.contacto, name="Contacto" ),
 ]
