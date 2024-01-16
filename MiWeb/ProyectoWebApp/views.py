3e3fa3488bcfad88134c7179ab4ac0295251bd6a
ISEATO-JR <julio.rivera.1596@gmail.com>
2024-01-15 23:51:59 -0600

    App Servicios

--- MiWeb/ProyectoWebApp/views.py
+++ MiWeb/ProyectoWebApp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+# Create your views here.   
 def inicio(request):
     return render(request, 'ProyectoWebApp/inicio.html')
 
---
+++ MiWeb/servicios/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import servicio
+
+# Register your models here.
+
+class ServiciosAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+
+admin.site.register(servicio,ServiciosAdmin) # Registramos la app de servicios en el panel de adminstracion
